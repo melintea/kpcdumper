ea97cf679e34a95c9b2c16b80ab9f8017f750045
Aurelian MELINTE <ame01@gmx.net>
2026-03-06 08:51:14 -0500

    cleanup

--- kpcdumper_lib.c
+++ kpcdumper_lib.c
@@ -45,7 +45,7 @@ void dump_core(const char* corefile, const char* devname)
     
     while (true == atomic_load(&_dumpdone)) {
         //printf("Waiting in %s...\n", corefile);
-    i   usleep(1000*100L);
+        usleep(1000*100L);
     } 
     //printf("Dumping %s\n", corefile);
     
